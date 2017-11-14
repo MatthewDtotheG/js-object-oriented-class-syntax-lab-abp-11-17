ddc8ac1838990547df4a323598f245b951b9aa30
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-14 02:06:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-function BoardMember(name, homeState, training) {
+class BoardMember {
+  constructor(name, homeState, training) {
   this.name = name;
   this.homeState = homeState;
   this.training = training;
-
+  };
   BoardMember.prototype.veto = function() {
     return 'No, I must disagree';
   };
